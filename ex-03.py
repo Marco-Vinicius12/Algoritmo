971c6a8dfb93c53a127f48d320e9764e3c5b267a
Vinicius <marco126498@gmail.com>
2025-03-20 19:16:43 -0300

    ex-03.py

---
+++ ex-03.py
@@ -0,0 +1,7 @@
+#03) Ler um valor e escrever a mensagem É MAIOR QUE 10! se o valor lido for maior que 10, caso contrário escrever NÃO É MAIOR QUE 10!
+print("Qual sua idade?")
+idade = int(input())
+if idade > 9:
+    print("É MAIOR QUE 10!")
+else:
+    print("NÃO É MAIOR QUE 10!")
