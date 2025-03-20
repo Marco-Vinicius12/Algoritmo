7ed2727391c26df17b4d8af9e6d877fd7b43f392
Vinicius <marco126498@gmail.com>
2025-03-20 19:30:43 -0300

    ex-04.py

---
+++ ex-04.py
@@ -0,0 +1,7 @@
+#04) Ler um valor e escrever se é positivo ou negativo (considere o valor zero como positivo).
+print("Informe um numero:")
+num = int(input())
+if num >= 0:
+    print("Seu valor é positivo")
+else:
+    print("Seu valor é negativo")
